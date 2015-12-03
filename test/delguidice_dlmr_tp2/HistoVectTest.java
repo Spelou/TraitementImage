f51f1cebeb01753cc496907a4721848c0085b3ac
Spelou <qdelamot@eleves.ec-nantes.fr>
2015-12-03 19:48:43 +0100

    Fin test histo

--- test/delguidice_dlmr_tp2/HistoVectTest.java
+++ test/delguidice_dlmr_tp2/HistoVectTest.java
@@ -17,24 +17,27 @@ import static org.junit.Assert.*;
  * @author Quentin
  */
 public class HistoVectTest {
-    
+
     public HistoVectTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
+        System.out.println("Début des tests sur histo");
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
+        System.out.println("Fin des test sur histo");
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
+        System.out.println("Test Ok");
     }
 
     /**
@@ -43,11 +46,31 @@ public class HistoVectTest {
     @Test
     public void testRemplissageVecteur() {
         System.out.println("remplissageVecteur");
-        int[][] M = null;
-        HistoVect instance = new HistoVect();
-        instance.remplissageVecteur(M);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        // création matrice particulière
+        int matriceTest[][];
+        matriceTest = new int[2][3];
+        matriceTest[0][0] = 255;
+        matriceTest[0][1] = 16;
+        matriceTest[0][2] = 40;
+        matriceTest[1][0] = 40;
+        matriceTest[1][1] = 16;
+        matriceTest[1][2] = 40;
+         //creation du vecteur voulu
+        // décalage des indices de 1 en comptant le 0
+        int[] vecteurVoulu;
+        vecteurVoulu = new int[256];
+        vecteurVoulu[255] = 1;
+        vecteurVoulu[40] = 3;
+        vecteurVoulu[16] = 2;
+
+        //création du vecteur à partir de la classe
+        HistoVect histoVect = new HistoVect();
+        histoVect.remplissageVecteur(matriceTest);
+
+        //assertion
+        for (int i = 0; i < 256; i++) {
+            assertEquals(vecteurVoulu[i], histoVect.getVecteur()[i]);
+        }
     }
 
     /**
@@ -55,11 +78,17 @@ public class HistoVectTest {
      */
     @Test
     public void testAfficheVecteur() {
-        System.out.println("afficheVecteur");
-        HistoVect instance = new HistoVect();
-        instance.afficheVecteur();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("Test affiche Vecteur");
+        int[] vecteurParticulier;
+        vecteurParticulier= new int [4];
+        vecteurParticulier[0]=1;
+        vecteurParticulier[1]=2;
+        vecteurParticulier[2]=3;
+        vecteurParticulier[3]=4;
+        HistoVect histoVect= new HistoVect(vecteurParticulier);
+        System.out.println("affichage du vecteur (1,2,3,4)");
+        histoVect.afficheVecteur();
+        
     }
 
     /**
@@ -68,10 +97,7 @@ public class HistoVectTest {
     @Test
     public void testAfficheVecteurDetailler() {
         System.out.println("afficheVecteurDetailler");
-        HistoVect instance = new HistoVect();
-        instance.afficheVecteurDetailler();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("Pareil que précédement");
     }
 
     /**
@@ -79,13 +105,7 @@ public class HistoVectTest {
      */
     @Test
     public void testGetVecteur() {
-        System.out.println("getVecteur");
-        HistoVect instance = new HistoVect();
-        int[] expResult = null;
-        int[] result = instance.getVecteur();
-        assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
     }
 
     /**
@@ -93,12 +113,7 @@ public class HistoVectTest {
      */
     @Test
     public void testSetVecteur() {
-        System.out.println("setVecteur");
-        int[] vecteur = null;
-        HistoVect instance = new HistoVect();
-        instance.setVecteur(vecteur);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
     }
 
     /**
@@ -107,12 +122,21 @@ public class HistoVectTest {
     @Test
     public void testMaximumVecteur() {
         System.out.println("maximumVecteur");
-        HistoVect instance = new HistoVect();
-        int expResult = 0;
-        int result = instance.maximumVecteur();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        int[] vecteurParticulier;
+        vecteurParticulier= new int [4];
+        vecteurParticulier[0]=1;
+        vecteurParticulier[1]=4;
+        vecteurParticulier[2]=2;
+        vecteurParticulier[3]=3;
+        HistoVect histoVect= new HistoVect(vecteurParticulier);
+        assertEquals(histoVect.maximumVecteur(),4);
+         int[] vecteurParticulier1;
+        vecteurParticulier1= new int [4];
+        vecteurParticulier1[0]=1;
+        vecteurParticulier1[1]=4;
+        vecteurParticulier1[2]=4;
+        vecteurParticulier1[3]=3;
+        assertEquals(histoVect.maximumVecteur(),4);
     }
-    
+
 }
